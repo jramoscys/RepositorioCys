6e15747f8fb4502151209b6afb810a3339263319
jramos <jramos@cystelcom.com>
2012-03-22 17:40:29 +0100

    Impresion feature1

--- PruebaGit/src/com/cystelcom/test/Impresor.java
+++ PruebaGit/src/com/cystelcom/test/Impresor.java
@@ -3,5 +3,6 @@ package com.cystelcom.test;
 public class Impresor {
 	public static void Imprime(){
 		System.out.println("Impresor que imprime");
+		System.out.print("esto es de la feature1");
 	}
 }
