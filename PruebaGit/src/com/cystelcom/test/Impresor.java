44e7c0d47bd3ff8e04014c0d5fdf9dee60773946
jramos <jramos@cystelcom.com>
2012-03-22 17:09:33 +0100

    Cambios en impresor

--- PruebaGit/src/com/cystelcom/test/Impresor.java
+++ PruebaGit/src/com/cystelcom/test/Impresor.java
@@ -2,7 +2,6 @@ package com.cystelcom.test;
 
 public class Impresor {
 	public static void Imprime(){
-		System.out.println("Imprimir, cambiar y mover.");
-		System.out.println("Imprimir Cambiado y ya esta");
+		System.out.println("Impresor que imprime");
 	}
 }
