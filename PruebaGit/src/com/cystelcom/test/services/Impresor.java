4a989d0f2b77e4459f6566a9038fee86b9ead880
jramos <jramos@cystelcom.com>
2012-03-22 16:43:15 +0100

    Impresora cambiada

--- PruebaGit/src/com/cystelcom/test/services/Impresor.java
+++ PruebaGit/src/com/cystelcom/test/services/Impresor.java
@@ -2,6 +2,6 @@ package com.cystelcom.test.services;
 
 public class Impresor {
 	public static void Imprime(){
-		System.out.println("Imprimir");
+		System.out.println("Imprimir Cambiado y ya esta");
 	}
 }
