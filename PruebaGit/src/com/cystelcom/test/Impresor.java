9529d0ae036a9e6ce557e8c52a2b566d1f6205c1
jramos <jramos@cystelcom.com>
2012-03-22 16:44:16 +0100

    Movido y cambiado el impresor

---
+++ PruebaGit/src/com/cystelcom/test/Impresor.java
@@ -0,0 +1,7 @@
+package com.cystelcom.test;
+
+public class Impresor {
+	public static void Imprime(){
+		System.out.println("Imprimir, cambiar y mover.");
+	}
+}
--- PruebaGit/src/com/cystelcom/test/services/Impresor.java
+++
@@ -1,7 +0,0 @@
-package com.cystelcom.test.services;
-
-public class Impresor {
-	public static void Imprime(){
-		System.out.println("Imprimir");
-	}
-}
