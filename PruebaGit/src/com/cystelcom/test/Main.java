7593f19210433dfdf8c4e7b9da74d7758364f06e
jramos <jramos@cystelcom.com>
2012-03-22 17:34:11 +0100

    Cambios en main

--- PruebaGit/src/com/cystelcom/test/Main.java
+++ PruebaGit/src/com/cystelcom/test/Main.java
@@ -6,12 +6,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//No se os olvide meter aquí vuestro código.
-		//Esto va a ser un chanchullo
 		System.out.println("Feature 2");
 		System.out.println("Feature 1");
-		System.out.println("Esta característica es desde fuera");
 	}
 
 }
