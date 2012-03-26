79c921f2f4463a2022b68e63f26686495b51af86
jramos <jramos@cystelcom.com>
2012-03-26 11:25:52 +0200

    Feature mergebranch

--- PruebaGit/src/com/cystelcom/testeo/Principal.java
+++ PruebaGit/src/com/cystelcom/testeo/Principal.java
@@ -9,6 +9,7 @@ public class Principal {
 		System.out.println("Feature 1");
 		System.out.println("Feature 2");
 		System.out.println("Feature 3");
+		System.out.println("Feature del mergebranch");
 	}
 
 }
